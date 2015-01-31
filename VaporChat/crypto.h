c690dc5987e56ef898fcb59dc07e6fd07bf49d37
Nathan Liebrecht <nwliebrecht@gmail.com>
2015-01-31 15:03:04 -0700

    First commit with crypto functionality. Currently can generate key pair locally.

---
+++ VaporChat/crypto.h
@@ -0,0 +1,8 @@
+#ifndef CRYPTO_H
+#define CRYPTO_H
+
+void init_crypto();
+void createKeyPair(char *password, char *pub_out, char *priv_out);
+
+#endif // CRYPTO_H
+
--- VaporChat/main.cpp
+++ VaporChat/main.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+
+#include "crypto.h"
+
+void nathan_tests() {
+    init_crypto();
+
+    char *pub, *priv;
+    createKeyPair("test", pub, priv);
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    nathan_tests();
 
     return a.exec();
 }
